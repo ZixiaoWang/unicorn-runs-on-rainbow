d94dbe66774852c80744e41bc84cc36fc092bd6d
Jacky Wang <zwang@flexport.com>
2022-06-14 10:45:54 +0800

    updated programs

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ window.checkTimeTimeout = 0;
 
 // const BG_CLOUDS = [1,2,3,4].map(index => `assets/bg-cloud-${index}.png`);
 // const FG_CLOUDS = [1,2,3,4,5].map(index => `assets/cloud-${index}.png`);
-const BG_CLOUDS = [1,2,3,4,5,6,7,8,9].map(index => `assets/realcloud${index}.png`);
-const FG_CLOUDS = [1,2,3,4,5,6,7,8,9].map(index => `assets/realcloud${index}.png`);
-const FLOAT_CLOUDS = [1,2,3,4,5,6,7,8,9,10].map(index => `assets/float-cloud-${index}.png`);
+const BG_CLOUDS = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+  (index) => `assets/realcloud${index}.png`
+);
+const FG_CLOUDS = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+  (index) => `assets/realcloud${index}.png`
+);
+const FLOAT_CLOUDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+  (index) => `assets/float-cloud-${index}.png`
+);
 const RAINBOW = "assets/rainbow.png";
 // const UNICORN = "assets/unicorn.gif";
 const UNICORN = "assets/unicorn_with_wings.gif";
 const COINS = "assets/coins.gif";
-const BACKGROUNDS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24].map(num => `assets/background-0${num}.jpg`);
+const BACKGROUNDS = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24,
+].map((num) => `assets/background-0${num}.jpg`);
 
 const BACKGROUND_CONTAINER = document.querySelector(".background");
 const BG_CLOUD_CONTAINER = document.querySelector(".bg-cloud");
@@ -29,53 +38,53 @@ const BULLETS_CONTAINER = document.querySelector(".bullets");
 const TEXT_COLORS = ["red", "orange", "yellow", "green", "cyan"];
 
 const generateImg = (paths, duration) => {
-    const img = document.createElement("img");
-    const animationDuration = duration || 1000;
-    
-    if (typeof paths === "string") {
-        img.src = paths;
-    } else {
-        const randomIndex = Math.floor(paths.length * Math.random());
-        img.src = paths[randomIndex];
-    }
+  const img = document.createElement("img");
+  const animationDuration = duration || 1000;
 
-    if (/rainbow\.png$|unicorn\.gif|unicorn_with_wings\.gif$/.test(img.src)) {
-        img.classList.add("static");
-    }
-    img.style.animationDuration = (animationDuration / 1000).toFixed(1) + "s";
-    return img;
-}
+  if (typeof paths === "string") {
+    img.src = paths;
+  } else {
+    const randomIndex = Math.floor(paths.length * Math.random());
+    img.src = paths[randomIndex];
+  }
+
+  if (/rainbow\.png$|unicorn\.gif|unicorn_with_wings\.gif$/.test(img.src)) {
+    img.classList.add("static");
+  }
+  img.style.animationDuration = (animationDuration / 1000).toFixed(1) + "s";
+  return img;
+};
 
 const generateBullet = (text, index) => {
-    const div = document.createElement("div");
-    div.classList.add("bullet");
-    div.textContent = text;
-    div.style.color = TEXT_COLORS[index % TEXT_COLORS.length];
-    return div;
-}
+  const div = document.createElement("div");
+  div.classList.add("bullet");
+  div.textContent = text;
+  div.style.color = TEXT_COLORS[index % TEXT_COLORS.length];
+  return div;
+};
 
 const makeItAnimated = (container, paths) => {
-    if (!container) {
-        return null;
-    }
+  if (!container) {
+    return null;
+  }
 
-    const duration = Math.round(Math.random() * 6000 + 6000);
-    const img = generateImg(paths, duration);
-    container.appendChild(img);
-    setTimeout(() => container.removeChild(img), duration * 2);
+  const duration = Math.round(Math.random() * 6000 + 6000);
+  const img = generateImg(paths, duration);
+  container.appendChild(img);
+  setTimeout(() => container.removeChild(img), duration * 2);
 
-    setTimeout(() => {
-        makeItAnimated(container, paths);
-    }, Math.round(duration / 2));
-}
+  setTimeout(() => {
+    makeItAnimated(container, paths);
+  }, Math.round(duration / 2));
+};
 
 const coinAnimated = (container) => {
-    const duration = Math.round(Math.random() * 5000 + 3000);
-    const img = generateImg(COINS, duration);
-    container.appendChild(img);
-    setTimeout(() => container.removeChild(img), duration * 2);
-    setTimeout(() => coinAnimated(container), Math.round(duration / 2));
-}
+  const duration = Math.round(Math.random() * 5000 + 3000);
+  const img = generateImg(COINS, duration);
+  container.appendChild(img);
+  setTimeout(() => container.removeChild(img), duration * 2);
+  setTimeout(() => coinAnimated(container), Math.round(duration / 2));
+};
 
 RAINBOW_CONTAINER.append(generateImg(RAINBOW));
 UNICORN_CONTAINER.append(generateImg(UNICORN));
@@ -92,43 +101,49 @@ coinAnimated(COINS_CONTAINER_2);
 coinAnimated(COINS_CONTAINER_3);
 
 const checkTime = (...targetTime) => {
-    window.clearTimeout(window.checkTimeTimeout)
-    const now = targetTime ? new Date(...targetTime) : new Date();
-    const hourIndex = now.getHours();
-    console.log({ now, hourIndex })
-    BACKGROUND_CONTAINER.style.backgroundImage = `url(${BACKGROUNDS[hourIndex]})`;
-    window.checkTimeTimeout = setTimeout(checkTime, 1000 * 60 * 30);
-}
+  window.clearTimeout(window.checkTimeTimeout);
+  const now = targetTime ? new Date(...targetTime) : new Date();
+  const hourIndex = now.getHours();
+  console.log({ now, hourIndex });
+  BACKGROUND_CONTAINER.style.backgroundImage = `url(${BACKGROUNDS[hourIndex]})`;
+  window.checkTimeTimeout = setTimeout(checkTime, 1000 * 60 * 30);
+};
 
 const updateClock = (showSemicolon) => {
-    const now = new Date();
-    const date = now.toLocaleDateString();
-    const timeFragments = now.toLocaleTimeString().split(/:|\s/).slice(0, 3);
-    const time = showSemicolon ? timeFragments.join(":") : timeFragments.join(" ");
-    document.getElementById("date").textContent = date;
-    document.getElementById("time").textContent = time;
-    setTimeout(() => updateClock(!showSemicolon), 500);
-}
+  const now = new Date();
+  const date = now.toLocaleDateString();
+  const timeFragments = now.toLocaleTimeString().split(/:|\s/).slice(0, 3);
+  const time = showSemicolon
+    ? timeFragments.join(":")
+    : timeFragments.join(" ");
+  document.getElementById("date").textContent = date;
+  document.getElementById("time").textContent = time;
+  setTimeout(() => updateClock(!showSemicolon), 500);
+};
 
 const updateData = () => {
-    const searchString = location.search.substring(1);
-    const searchfragments = searchString.split("&");
-    const query = {};
+  const searchString = location.search.substring(1);
+  const searchfragments = searchString.split("&");
+  const query = {};
 
-    searchfragments.forEach(fragment => {
-        const keyvaluepair = fragment.split("=");
-        const key = keyvaluepair[0]?.toLocaleLowerCase();
-        const value = keyvaluepair[1]?.toLocaleLowerCase();
-        query[key] = value;
-    });
+  searchfragments.forEach((fragment) => {
+    const keyvaluepair = fragment.split("=");
+    const key = keyvaluepair[0]?.toLocaleLowerCase();
+    const value = keyvaluepair[1]?.toLocaleLowerCase();
+    query[key] = value;
+  });
 
-    document.getElementById("participants").textContent = query["participant"] || query["participants"] || 0;
-    document.getElementById("projects").textContent = query["project"] || query["projects"] || 0;
-    document.getElementById("donation").textContent = query["donation"] || query["donations"] || 0;
-}
+  document.getElementById("participants").textContent =
+    query["participant"] || query["participants"] || 0;
+  document.getElementById("projects").textContent =
+    query["project"] || query["projects"] || 0;
+  document.getElementById("donation").textContent =
+    query["donation"] || query["donations"] || 0;
+};
 
 const renderButtlets = () => {
-    const projects = [`Violette's Bedtime Stories`,
+  const projects = [
+    `Violette's Bedtime Stories`,
     `Sophia's cold brew coffee`,
     `Ryan's free ride service`,
     `Public Speaking with Ken`,
@@ -256,25 +271,35 @@ const renderButtlets = () => {
     `Cassie's Climbing(攀岩) Class 🧗‍♀️`,
     `Tracy's Fruit Milkshake Shop`,
     `Sailing Class (J80 class tournament) `,
-    `Flex and Relax with Cathy`];
-    
-    projects.forEach((project, index) => {
-        const div = generateBullet(project, index);
-        BULLETS_CONTAINER.appendChild(div);
-    });
+    `Flex and Relax with Cathy`,
+    `SQL 101`,
+    `纯K`,
+    `Dance With Me: take Ballet Class at 中央芭蕾舞团`,
+    `Water sports with Joyce`,
+    `part-time teaching sharing`,
+    `Mojito Happy Hour with Emily`,
+  ];
 
-    let left = 0;
-    const move = () => {
-        BULLETS_CONTAINER.style.left = left + "px";
-        left -= 4;
-        if (left <= -1 * (BULLETS_CONTAINER.getBoundingClientRect().width - window.innerWidth)) {
-            left = 0;
-        }
-        window.requestAnimationFrame(move);
+  projects.forEach((project, index) => {
+    const div = generateBullet(project, index);
+    BULLETS_CONTAINER.appendChild(div);
+  });
+
+  let left = 0;
+  const move = () => {
+    BULLETS_CONTAINER.style.left = left + "px";
+    left -= 4;
+    if (
+      left <=
+      -1 * (BULLETS_CONTAINER.getBoundingClientRect().width - window.innerWidth)
+    ) {
+      left = 0;
     }
+    window.requestAnimationFrame(move);
+  };
 
-    move();
-}
+  move();
+};
 
 checkTime();
 updateClock();
